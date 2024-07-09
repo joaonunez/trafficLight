fb7d2765b620e0b38bb113470839e9b9de1df567
joaonunez <joaovaldiglesias@gmail.com>
2024-07-09 11:28:17 -0400

    proyecto avanzado

--- app/src/components/Semaforo.jsx
+++ app/src/components/Semaforo.jsx
@@ -1,14 +1,31 @@
-import React from "react";
-import { useState, useEffect } from "react";
-export function Semaforo(){
+import React, { useState, useEffect } from "react";
+export function Semaforo() {
+  const [color, setColor] = useState(null); 
+
+  useEffect(() => {
     
-    return(
-        <>
-            <div className="cuerpo-semaforo">
-                <div className="rojo"></div>
-                <div className="amarillo"></div>
-                <div className="verde"></div>
-            </div>
-        </>
-    );
+  },[color]); 
+
+  const handleClick = (selectedColor) => {
+    setColor(selectedColor); 
+  };
+
+  return (
+    <>
+      <div className="cuerpo-semaforo">
+        <div
+          className={`rojo ${color === "rojo" ? "seleccionado" : ""}`}
+          onClick={() => handleClick("rojo")}
+        ></div>
+        <div
+          className={`amarillo ${color === "amarillo" ? "seleccionado" : ""}`}
+          onClick={() => handleClick("amarillo")}
+        ></div>
+        <div
+          className={`verde ${color === "verde" ? "seleccionado" : ""}`}
+          onClick={() => handleClick("verde")}
+        ></div>
+      </div>
+    </>
+  );
 }
